d3aa428e79e2126c996b49409a20c6e1b9b2f62c
KhubaibKhan4 <18.bscs.803@gmail.com>
2025-01-25 18:02:37 +0500

    -Features Cleanup Impl

--- Notes/ContentView.swift
+++ Notes/ContentView.swift
@@ -13,9 +13,6 @@ struct ContentView: View {
     @State private var isSelected : Int = 0
     @AppStorage("isPermissionGranted") var isPermissionGranted = false
     var body: some View {
-        if !isPermissionGranted {
-            PermissionScreen()
-        }else {
             TabView(selection:$isSelected) {
                 Tab("Home", systemImage: "house",value: 0) {
                     HomeScreen()
@@ -31,7 +28,6 @@ struct ContentView: View {
                 }
 
             }
-        }
     }
 }
 
